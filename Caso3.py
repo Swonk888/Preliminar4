9f52e35bb576f5ec994cccdece4f689861289dfe
Kevin Chang <kchang@estudiantec.cr>
2023-05-10 20:10:08 -0600

    Varios productos+Otras funciones

--- Caso3.py
+++ Caso3.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import pyodbc
+from tkinter import messagebox
 
 #cursor.execute('SELECT * FROM contratos')
 
@@ -26,15 +27,15 @@ class VentanaRegistroVentas(tk.Frame):
         self.ganancias = []
         self.compra = []
         self.productos = []
-        self.contrato = 0
-        self.tipo_cambio = 1.0
-        self.simbolo = '₡'
+        self.contrato = tk.StringVar(value='0')
+        self.tipo_cambio = tk.StringVar(value='1')
+        self.simbolo = tk.StringVar(value='₡')
         
         cursor.execute('SELECT descripcion FROM productos')
         productos = cursor.fetchall()
         cursor.execute('SELECT nombre FROM monedas')
         monedas = cursor.fetchall()
-        self.moneda = tk.StringVar(value=monedas[0])
+        self.moneda = tk.StringVar(value='Seleccione una moneda')
 
         tk.Label(self, text='Moneda:').grid(row=1, column=0)
         tk.OptionMenu(self, self.moneda, *[row[0] for row in monedas], command=self.actualizar_moneda).grid(row=1, column=1)
@@ -52,13 +53,13 @@ class VentanaRegistroVentas(tk.Frame):
         tk.Label(self, text='Precio Unitario:').grid(row=4, column=0)
         tk.Label(self, textvariable = self.precio).grid(row=4, column=1)
 
-        tk.Label(self, text='Monto total:').grid(row=6, column=0)
-        tk.Label(self, textvariable = self.costo).grid(row=6, column=1)
+        tk.Label(self, text='Monto total:').grid(row=5, column=0)
+        tk.Label(self, textvariable = self.costo).grid(row=5, column=1)
 
         
 
         # botón de calcular costo
-        tk.Button(self, text='Agregar a Carrito', command=self.calcular_costo).grid(row=5, column=0)
+        tk.Button(self, text='Agregar a Carrito', command=self.calcular_costo).grid(row=8, column=1)
 
         # botón de registro de venta
         tk.Button(self, text='Registrar venta', command=self.guardar_venta).grid(row=8, column=2)
@@ -73,7 +74,7 @@ class VentanaRegistroVentas(tk.Frame):
         cursor = conn.cursor()
         cursor.execute('SELECT tipo_cambio_actual FROM monedas WHERE nombre = ?', moneda)
         tipo_cambio = cursor.fetchone()[0]
-        self.tipo_cambio.set(float(tipo_cambio))
+        self.tipo_cambio.set(str(tipo_cambio))
         cursor.execute('SELECT simbolo FROM monedas WHERE nombre = ?', moneda)
         simbolo = cursor.fetchone()[0]
         self.simbolo.set(simbolo)
@@ -87,34 +88,37 @@ class VentanaRegistroVentas(tk.Frame):
         
 
         moneda = self.moneda.get()
-        tipo_cambio = self.tipo_cambio.get()
+        tipo_cambio = float(self.tipo_cambio.get())
         nombreP = self.nombre_producto.get()
         cantidad = float(self.cantidad.get())
-        precio = float(self.precio.get())
+        precio0 = (self.precio.get())[1:-1]
+        precio = float(precio0)
         costo =  float(self.costo.get())
         costo = costo + (precio * cantidad)
+        simbolo = self.simbolo.get()
         self.costo.set(str(costo))
 
         cursor.execute('SELECT moneda_id FROM monedas WHERE nombre = ?', moneda)
         moneda_id = cursor.fetchone()[0]
-        cursor.execute('SELECT tipo_cambio_id FROM tipo_cambio WHERE tipo_cambio = ? and moneda_id = ?', tipo_cambio, moneda)
+        cursor.execute('SELECT tipo_cambio_id FROM tipo_cambio WHERE tipo_cambio = ? and moneda_id = ?', tipo_cambio, moneda_id)
         tipo_cambio_id = cursor.fetchone()[0]
-        productos = self.productos.get()
-        productos.append([nombreP, cantidad, precio, moneda_id, tipo_cambio_id])
-        self.productos.set(productos)
+        self.productos.append([nombreP, cantidad, precio, moneda_id, tipo_cambio_id])
 
-        contrato = self.contrato.get()
+        contrato =int(self.contrato.get())
         costo2 = float(self.costoP.get())
-        gananciasActuales = self.ganancias.get()
         ganancias =  costo - costo2
-        gananciasActuales.append([ganancias, contrato])
-        self.ganancias.set(gananciasActuales)
+        self.ganancias.append([ganancias, contrato])
 
-        compraActual = (self.compra.get())
+        
         producto = (self.nombre_producto.get())
-        compraActual.append(producto)
+        self.compra.append(producto)
 
-        self.compra.set(compraActual)
+        cantidadP = float(self.cantidad_disponible.get()) - cantidad
+        cursor.execute('SELECT producto_id FROM productos WHERE descripcion = ?', nombreP)
+        producto_id = cursor.fetchall()[0][0]
+        cursor.execute('UPDATE productos_producidos set cantidad =? where producto_id = ?', cantidadP,producto_id)
+        conn.commit()
+        self.actualizar_cantidad(nombreP)
 
 
     def actualizar_precio(self, producto):
@@ -124,10 +128,10 @@ class VentanaRegistroVentas(tk.Frame):
         conn =  pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};TRUSTED_CONNECTION=yes;')
         cursor = conn.cursor()
         cursor.execute('SELECT precio FROM productos WHERE descripcion = ?', producto)
-        precio = cursor.fetchall()[0][0]
-        tipo_cambio = self.tipo_cambio.get()
+        precio = float(cursor.fetchall()[0][0])
+        tipo_cambio = float(self.tipo_cambio.get())
         simbolo = self.simbolo.get()
-        precio =  precio * tipo_cambio
+        precio =  precio / tipo_cambio
 
         # actualizar la variable de cantidad disponible y la etiqueta correspondiente
         self.precio.set(simbolo + str(precio))
@@ -142,9 +146,9 @@ class VentanaRegistroVentas(tk.Frame):
         cursor = conn.cursor()
         cursor.execute('SELECT producto_id FROM productos WHERE descripcion = ?', producto)
         producto_id = cursor.fetchall()[0][0]
-        cursor.execute('SELECT contrato_id FROM productos_producidos WHERE producto_id = ?', producto)
+        cursor.execute('SELECT contrato_id FROM productos_producidos WHERE producto_id = ?', producto_id)
         contrato_id = cursor.fetchall()[0][0]
-        self.contrato.set(contrato_id)
+        self.contrato.set(str(contrato_id))
         cursor.execute('SELECT proceso_id FROM proceso WHERE contrato_id = ?', contrato_id)
         proceso_id = cursor.fetchall()[0][0]
         cursor.execute('SELECT costo FROM proceso WHERE proceso_id = ?', proceso_id)
@@ -166,9 +170,6 @@ class VentanaRegistroVentas(tk.Frame):
         ventana_montos.title('Montos')
         ventana_montos.geometry('300x200')
 
-        lista_compra = self.compra.get()
-        lista_ganancias = self.ganancias.get()
-
 
         # conectar a la base de datos
         server = 'localhost'
@@ -179,7 +180,7 @@ class VentanaRegistroVentas(tk.Frame):
 
 
         # Obtener el contrato correspondiente al producto
-        for i, gana in enumerate(lista_ganancias):
+        for i, gana in enumerate(self.ganancias):
             cursor = conn.cursor()
             cursor.execute('SELECT actor_id FROM actores_x_contrato WHERE contrato_id = ?', gana[1])
             actores = cursor.fetchall()
@@ -199,10 +200,9 @@ class VentanaRegistroVentas(tk.Frame):
             montoR = gana[0] * float(porcentajeR)
             ganancia =  gana[0]
             ganancia = ganancia - montoR
-            
             # Mostrar los montos en una lista
-            tk.Label(ventana_montos, text=nombre).grid(row=1, column=0)
-            tk.Label(ventana_montos, text=str(montoR)).grid(row=1, column=1)
+            tk.Label(ventana_montos, text=nombre).grid(row=2*i + 1+ len(actores)*i, column=0)
+            tk.Label(ventana_montos, text=str(montoR)).grid(row=2*i + 1+ len(actores)*1, column=1)
 
             for j, actor in enumerate(actores):
                 cursor.execute('SELECT descripcion FROM actores WHERE actor_id = ?', actor[1])
@@ -212,8 +212,8 @@ class VentanaRegistroVentas(tk.Frame):
                 tk.Label(ventana_montos, text=nombreA).grid(row=(len(actores))*i+j+1, column=0)
                 tk.Label(ventana_montos, text=str(montoN)).grid(row=(len(actores))*i+j+1, column=1)
             montoE = ganancia
-            tk.Label(ventana_montos, text='Esencial Verde').grid(row=2, column=0)
-            tk.Label(ventana_montos, text=str(montoR)).grid(row=2, column=1)
+            tk.Label(ventana_montos, text='Esencial Verde').grid(row=2*i + 2+ len(actores), column=0)
+            tk.Label(ventana_montos, text=str(montoE)).grid(row=2*i + 2+ len(actores), column=1)
 
 
     def guardar_venta(self):
@@ -224,15 +224,15 @@ class VentanaRegistroVentas(tk.Frame):
         cursor = conn.cursor()
 
         #productos.append([nombreP, cantidad, precio, moneda_id, tipo_cambio_id])
-        productos = self.productos.get()
-        for i, venta in enumerate(productos):
+    
+        for i, venta in enumerate(self.productos):
             # Obtener los valores de los campos de entrada
             nombre_producto = venta[0]
             cantidad = venta[1]
             precio = venta[2]
             moneda = venta[3]
             tipo_cambio = venta[4]
-            cursor.execute('SELECT producto_id FROM productos WHERE descripción = ?', nombre_producto)
+            cursor.execute('SELECT producto_id FROM productos WHERE descripcion = ?', nombre_producto)
             producto_id = cursor.fetchone()[0]
 
             # Calcular el monto de la venta
@@ -241,11 +241,11 @@ class VentanaRegistroVentas(tk.Frame):
             fecha = '2023-05-12 07:00:00'    
 
             # Registrar Venta
-            cursor.execute('INSERT INTO ventas (producto_id, monto, fecha, cantidad, moneda_id, tipo_cambio_id) values (?, ?, ?, ?, ?, ?)', producto_id, monto_venta, cantidad, moneda, tipo_cambio)
+            cursor.execute('INSERT INTO ventas (producto_id, monto, fecha, cantidad, moneda_id, tipo_cambio_id) values (?, ?, ?, ?, ?, ?)', producto_id, monto_venta, fecha, cantidad, moneda, tipo_cambio)
             conn.commit()
 
         # Mostrar un mensaje de confirmación
-        tk.messagebox.showinfo('Venta guardada', f'Se ha registrado una venta por un monto total de {monto_venta}.')
+        messagebox.showinfo('Venta guardada', f'Se ha registrado una venta por un monto total de {self.simbolo.get()} {monto_venta}.')
 
 
 root = tk.Tk()
